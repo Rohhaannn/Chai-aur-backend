56e90f721bd5e8b164da606455fed2f50a5ac77f
Rohan <shingaderohan96@gmail.com>
2024-04-04 20:35:57 +0530

    index.js code updated

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("chai aur code");
+const express = require('express')
+const app = express()
+const port = 4000
+
+
+app.get('/', (req, res)=>{
+  res.send("Hello World");
+})
+
+app.get('/twitter', (req, res) => {
+  res.send("I_am_rohan")
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1> Please login @ chai aur code </h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2> Chai Aue Code </h2>')
+})
+
+app.listen(port, ()=>{
+  console.log(`Example app listening on port ${port}`)
+})
